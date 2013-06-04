ec77a6e672571913a835cd6a7a3372d40c64cb24
monkeymonkey <teamjrann2@gmail.com>
2013-06-04 15:31:04 -0500

    initial commit of orange app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orange::Application.config.secret_token = 'c956ba4223179b70ba616c943f1cb312cd93cb3e2b5cd7e14eb11a76703f401498fa5a6f7768b828cfaf337254c7743115b2a334358dec0ef23fe357e79daead'
